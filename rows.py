d3081f8abd4fbe138cf7ef23c7a74bdfa0ec1d96
roccas28 <roccas.raveendran@torontomu.ca>
2026-05-31 20:28:48 -0400

    Modification - row.py not needed
    Modification - row.py not needed

--- rows.py
+++
@@ -1,5 +0,0 @@
-import pyarrow.parquet as pq
-
-# Read only the metadata, not the data itself
-metadata = pq.read_metadata('./output/processed_sequences.parquet')
-print(f"Total sequences (users) ready for ML: {metadata.num_rows}")
